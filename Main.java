9944e971fee6ed8448fb155a5d3e76016f26015f
TejaswiniH123 <tejaswinih9@gmail.com>
2025-12-06 20:33:15 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        {
+        System.out.println("This is inside the block.");
+        }
+    System.out.println("This is outside the block.");
+    }
+}
