77dcbbb6c9268a9e3ccdfba998f9cb123eb021df
TejaswiniH123 <tejaswinih9@gmail.com>
2025-12-12 15:18:59 +0530

    java tutorial

--- Main.java
+++ Main.java
@@ -257,21 +257,49 @@
 
 
 //conditional statements 
+// public class Main{
+//     public static void main(String[] args){
+//         int num = 10;
+//         // If-Else Statement
+//         if(num > 0){
+//             System.out.println("The number is positive.");
+//         } else if(num < 0){
+//             System.out.println("The number is negative.");
+//         } else{
+//             System.out.println("The number is zero.");
+//         }
+//     }
+// }
 public class Main{
     public static void main(String[] args){
-        int num = 10;
-        // If-Else Statement
-        if(num > 0){
-            System.out.println("The number is positive.");
-        } else if(num < 0){
-            System.out.println("The number is negative.");
-        } else{
-            System.out.println("The number is zero.");
+        int day = 3;
+        // Switch Statement
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
         }
     }
 }
-
-
-
 
 //looping statements in Java
