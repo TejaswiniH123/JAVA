4cd3cae8f3dc4b0f073020d35db0494c42a59e85
TejaswiniH123 <tejaswinih9@gmail.com>
2025-12-12 15:20:25 +0530

    java tutorial

--- Main.java
+++ Main.java
@@ -276,16 +276,16 @@ public class Main{
         // Switch Statement
         switch(day){
             case 1:
-            //     System.out.println("Monday");
-            //     break;
-            // case 2:
-            //     System.out.println("Tuesday");
-            //     break;
-            // case 3:
-            //     System.out.println("Wednesday");
-            //     break;
-            // case 4:
-            //     System.out.println("Thursday");
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
                 break;
             case 5:
                 System.out.println("Friday");
