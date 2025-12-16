94145204edd233b8b7cfe60457c87118c043d552
TejaswiniH123 <tejaswinih9@gmail.com>
2025-12-16 21:57:29 +0530

    java tutorial

--- Main.java
+++ Main.java
@@ -306,19 +306,19 @@
 //looping statements in Java
 public class Main{
     public static void main(String[] args){
-        // For Loop
-        System.out.println("For Loop:");
-        for(int i = 1; i <= 5; i++){
-            System.out.println(i);
-        }
-
-        // // While Loop
-        // System.out.println("While Loop:");
-        // int j = 1;
-        // while(j <= 5){
-        //     System.out.println(j);
-        //     j++;
+        // // For Loop
+        // System.out.println("For Loop:");
+        // for(int i = 1; i <= 5; i++){
+        //     System.out.println(i);
         // }
+
+        // While Loop
+        System.out.println("While Loop:");
+        int j = 1;
+        while(j <= 5){
+            System.out.println(j);
+            j++;
+        }
 
         // Do-While Loop
         // System.out.println("Do-While Loop:");
