bb9e7ebea9b1cdb2c8ededbae45199b9cdc72521
TejaswiniH123 <tejaswinih9@gmail.com>
2025-12-16 21:58:03 +0530

    java tutorial

--- Main.java
+++ Main.java
@@ -307,18 +307,18 @@
 public class Main{
     public static void main(String[] args){
         // // For Loop
-        // System.out.println("For Loop:");
-        // for(int i = 1; i <= 5; i++){
-        //     System.out.println(i);
-        // }
+        System.out.println("For Loop:");
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
 
         // While Loop
-        // System.out.println("While Loop:");
-        // int j = 1;
-        // while(j <= 5){
-        //     System.out.println(j);
-        //     j++;
-        // }
+        System.out.println("While Loop:");
+        int j = 1;
+        while(j <= 5){
+            System.out.println(j);
+            j++;
+        }
 
         // Do-While Loop
         System.out.println("Do-While Loop:");
