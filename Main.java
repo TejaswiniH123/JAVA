997d37b4dbbf735cd933d1aab67914c453aa1edf
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-07 12:20:05 +0530

    java tutorial

--- Main.java
+++ Main.java
@@ -331,13 +331,29 @@
 // }
 
 //Arrays in Java
-public class Main{
-    public static void main(String[] args){
-        int[] arr={10,20,30,40,50};
-        int n=arr.length;
-        System.out.println("Array elements are:");
-        for(int i=0;i<n;i++){
-            System.out.println(arr[i]);
-        }
-        }
+// public class Main{
+//     public static void main(String[] args){
+//         int[] arr={10,20,30,40,50};
+//         int n=arr.length;
+//         System.out.println("Array elements are:");
+//         for(int i=0;i<n;i++){
+//             System.out.println(arr[i]);
+//         }
+//         }
+// }
+
+//arrays in methods
+public class Main {
+    public static void main(String args[])
+    {
+        int arr[] = { 3, 1, 2, 5, 4 };
+        sum(arr);
+    }
+    public static void sum(int[] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++)
+            sum += arr[i];
+        System.out.println("sum of array values : " + sum);
+    }
 }
