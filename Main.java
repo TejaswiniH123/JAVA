52d112673ad8d82640053dd9f97bc5f35dddee83
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-07 13:15:44 +0530

    java tutorial

--- Main.java
+++ Main.java
@@ -359,15 +359,43 @@
 // }
 
 // Two-Dimensional Array
-public class Main {
-    public static void main(String args[]) {
-        int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        System.out.println("Two-Dimensional Array elements are:");
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+// public class Main {
+//     public static void main(String args[]) {
+//         int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+//         System.out.println("Two-Dimensional Array elements are:");
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = 0; j < arr[i].length; j++) {
+//                 System.out.print(arr[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+import java.util.Scanner;
+public class Main 
+{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+      	int row = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int col = sc.nextInt();
+        int[][] arr= new int[row][col];
+        System.out.println("Enter elements of array: ");
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j]= sc.nextInt();
+            }
+        }
+         System.out.println("Elements of array are: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arr[i][j]+ " ");
             }
             System.out.println();
         }
+        sc.close();
     }
 }
