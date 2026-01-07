c8ecca47d307290b5dbc79867c895012db9c1133
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-07 13:01:52 +0530

    java tutorial

--- Main.java
+++ Main.java
@@ -343,17 +343,31 @@
 // }
 
 //arrays in methods
+// public class Main {
+//     public static void main(String args[])
+//     {
+//         int arr[] = { 3, 1, 2, 5, 4 };
+//         sum(arr);
+//     }
+//     public static void sum(int[] arr)
+//     {
+//         int sum = 0;
+//         for (int i = 0; i < arr.length; i++)
+//             sum += arr[i];
+//         System.out.println("sum of array values : " + sum);
+//     }
+// }
+
+// Two-Dimensional Array
 public class Main {
-    public static void main(String args[])
-    {
-        int arr[] = { 3, 1, 2, 5, 4 };
-        sum(arr);
-    }
-    public static void sum(int[] arr)
-    {
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++)
-            sum += arr[i];
-        System.out.println("sum of array values : " + sum);
+    public static void main(String args[]) {
+        int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        System.out.println("Two-Dimensional Array elements are:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
