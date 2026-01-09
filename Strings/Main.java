13484e187bf7266c4d9e78a8023476e6cd23c529
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-09 13:09:35 +0530

    java tutorial

---
+++ Strings/Main.java
@@ -0,0 +1,8 @@
+//String example in java 
+public class Main {
+    public static void main(String args[])
+    {
+        String str = new String("Geeks");
+        System.out.println(str);
+    }
+}
