d86b70ea9711d63f150630eb545cd9e6c56f2e68
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-09 16:00:18 +0530

    java tutorial

--- Strings/Main.java
+++ Strings/Main.java
@@ -18,31 +18,42 @@
 //     }
 // }
 
+// public class Main {
+//     public static void main(String[] args) {
+//         // Both s1 and s2 refer to the same
+//         // string literal in the String Pool
+//         String s1 = "Hello";
+//         String s2 = "Hello";
+
+//         // true, both point to the same object in String Pool
+//         System.out.println("s1 == s2: " + (s1 == s2)); 
+
+//         // Concatenation creates a new String
+//         // object in heap, s1 now points to it
+//         s1 = s1.concat(" World");
+
+//         System.out.println("s1: " + s1);      
+//         System.out.println("s2: " + s2);     
+//         System.out.println("s1 == s2: " + (s1 == s2)); 
+        
+//         // Creating a string using new keyword stores it in the heap
+//         String s3 = new String("Hello");
+
+//         // false, because s2 is from String Pool and s3 is from heap
+//         System.out.println("s2 == s3: " + (s2 == s3)); 
+
+//         // true, because equals() compares content
+//         System.out.println("s2.equals(s3): " + s2.equals(s3));
+//     }
+// }
+
+// Combining Two Strings with concat() Method
 public class Main {
-    public static void main(String[] args) {
-        // Both s1 and s2 refer to the same
-        // string literal in the String Pool
-        String s1 = "Hello";
-        String s2 = "Hello";
-        
-        // true, both point to the same object in String Pool
-        System.out.println("s1 == s2: " + (s1 == s2)); 
-
-        // Concatenation creates a new String
-        // object in heap, s1 now points to it
-        s1 = s1.concat(" World");
-
-        System.out.println("s1: " + s1);      
-        System.out.println("s2: " + s2);     
-        System.out.println("s1 == s2: " + (s1 == s2)); 
-        
-        // Creating a string using new keyword stores it in the heap
-        String s3 = new String("Hello");
-
-        // false, because s2 is from String Pool and s3 is from heap
-        System.out.println("s2 == s3: " + (s2 == s3)); 
-
-        // true, because equals() compares content
-        System.out.println("s2.equals(s3): " + s2.equals(s3));
+    public static void main(String args[])
+    {
+        String str1 = "Hello, ";
+        String str2 = "World!";
+        String str3 = str1.concat(str2);
+        System.out.println("Combined String: " + str3);
     }
 }
