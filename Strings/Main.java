3a5321c23f9dc5bccd72757aade4b240400e5a6d
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-09 14:41:52 +0530

    java tutorial

--- Strings/Main.java
+++ Strings/Main.java
@@ -1,8 +1,19 @@
 //String example in java 
+// public class Main {
+//     public static void main(String args[])
+//     {
+//         String str = new String("Geeks");
+//         System.out.println(str);
+//     }
+// }
+
+// Example Demonstrating Immutability of Strings in Java
 public class Main {
     public static void main(String args[])
     {
         String str = new String("Geeks");
-        System.out.println(str);
+        System.out.println("String before modification: " + str);
+        str.concat("forGeeks");
+        System.out.println("String after modification attempt: " + str);
     }
 }
