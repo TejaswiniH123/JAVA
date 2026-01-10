2cbb7315a86dcf6cbf80850508dd7b7a9924edfe
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-10 20:16:08 +0530

    java tutorial

--- OOPS/ALL/Constructors/Main.java
+++ OOPS/ALL/Constructors/Main.java
@@ -1,10 +1,37 @@
 import java.io.*;
+
+//default constructor example
+// class Geeks{
+//     // Default Constructor
+//     Geeks(){
+//         System.out.println("Default constructor"); 
+//     }
+//     public static void main(String[] args){
+//         Geeks hello = new Geeks();
+//     }
+// }
+
+//parameterized constructor example
 class Geeks{
-    // Default Constructor
-    Geeks(){
-        System.out.println("Default constructor"); 
+    // data members of the class
+    String name;
+    int id;
+    // Parameterized Constructor
+    Geeks(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
     }
-    public static void main(String[] args){
-        Geeks hello = new Geeks();
+    // Method to display object data
+    void display(){   
+        System.out.println("GeekName: " + name
+                           + " and GeekId: " + id);
+    }
+    // main() method — placed inside the same class for
+    // universal compatibility
+    public static void main(String[] args){   
+        // This will invoke the parameterized constructor
+        Geeks geek1 = new Geeks("Sweta", 68);
+        geek1.display();
     }
 }
