0cde45c2b9ce7a8be9cf3b7248a17d3593b9c893
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-10 20:42:27 +0530

    java tutorial

--- OOPS/ALL/Constructors/Main.java
+++ OOPS/ALL/Constructors/Main.java
@@ -1,100 +1,100 @@
-// import java.io.*;
+import java.io.*;
 
-// //default constructor example
-// class Geeks{
-//     // Default Constructor
-//     Geeks(){
-//         System.out.println("Default constructor"); 
-//     }
-//     public static void main(String[] args){
-//         Geeks hello = new Geeks();
-//     }
-// }
-
-
-// //parameterized constructor example
-// class Geeks{
-//     // data members of the class
-//     String name;
-//     int id;
-//     // Parameterized Constructor
-//     Geeks(String name, int id)
-//     {
-//         this.name = name;
-//         this.id = id;
-//     }
-//     // Method to display object data
-//     void display(){   
-//         System.out.println("GeekName: " + name
-//                            + " and GeekId: " + id);
-//     }
-//     // main() method — placed inside the same class for
-//     // universal compatibility
-//     public static void main(String[] args){   
-//         // This will invoke the parameterized constructor
-//         Geeks geek1 = new Geeks("Sweta", 68);
-//         geek1.display();
-//     }
-// }
+//default constructor example
+class Geeks{
+    // Default Constructor
+    Geeks(){
+        System.out.println("Default constructor"); 
+    }
+    public static void main(String[] args){
+        Geeks hello = new Geeks();
+    }
+}
 
 
-// //COPY CONSTRUCTOR example
-// import java.io.*;
-// class Geeks{
-//     // data members of the class
-//     String name;
-//     int id;
-//     // Parameterized Constructor
-//     Geeks(String name, int id)
-//     {
-//         this.name = name;
-//         this.id = id;
-//     }
-//     // Copy Constructor
-//     Geeks(Geeks obj2)
-//     {
-//         this.name = obj2.name;
-//         this.id = obj2.id;
-//     }
-// }
-// class GFG {
-//     public static void main(String[] args)
-//     {
-//         // This would invoke the parameterized constructor
-//         System.out.println("First Object");
-//         Geeks geek1 = new Geeks("Sweta", 68);
-//         System.out.println("GeekName: " + geek1.name
-//                            + " and GeekId: " + geek1.id);
-
-//         System.out.println();
-//         // This would invoke the copy constructor
-//         Geeks geek2 = new Geeks(geek1);
-//         System.out.println(
-//             "Copy Constructor used Second Object");
-//         System.out.println("GeekName: " + geek2.name
-//                            + " and GeekId: " + geek2.id);
-//     }
-// }
+//parameterized constructor example
+class Geeks{
+    // data members of the class
+    String name;
+    int id;
+    // Parameterized Constructor
+    Geeks(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+    // Method to display object data
+    void display(){   
+        System.out.println("GeekName: " + name
+                           + " and GeekId: " + id);
+    }
+    // main() method — placed inside the same class for
+    // universal compatibility
+    public static void main(String[] args){   
+        // This will invoke the parameterized constructor
+        Geeks geek1 = new Geeks("Sweta", 68);
+        geek1.display();
+    }
+}
 
 
-// //PRIVATE CONSTRUCTOR example
-// class GFG {
-//     // Private constructor
-//     private GFG(){
-//         System.out.println("Private constructor called");
-//     }
-//     // Static method
-//     public static void displayMessage(){        
-//         System.out.println("Hello from GFG class!");
-//     }
-// }
-// class Main{   
-//     public static void main(String[] args){
-//         // GFG u = new GFG(); // Error: constructor is
-//         // private
-//        GFG.displayMessage();
-//     }
-// }
+//COPY CONSTRUCTOR example
+import java.io.*;
+class Geeks{
+    // data members of the class
+    String name;
+    int id;
+    // Parameterized Constructor
+    Geeks(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+    // Copy Constructor
+    Geeks(Geeks obj2)
+    {
+        this.name = obj2.name;
+        this.id = obj2.id;
+    }
+}
+class GFG {
+    public static void main(String[] args)
+    {
+        // This would invoke the parameterized constructor
+        System.out.println("First Object");
+        Geeks geek1 = new Geeks("Sweta", 68);
+        System.out.println("GeekName: " + geek1.name
+                           + " and GeekId: " + geek1.id);
+
+        System.out.println();
+        // This would invoke the copy constructor
+        Geeks geek2 = new Geeks(geek1);
+        System.out.println(
+            "Copy Constructor used Second Object");
+        System.out.println("GeekName: " + geek2.name
+                           + " and GeekId: " + geek2.id);
+    }
+}
+
+
+//PRIVATE CONSTRUCTOR example
+class GFG {
+    // Private constructor
+    private GFG(){
+        System.out.println("Private constructor called");
+    }
+    // Static method
+    public static void displayMessage(){        
+        System.out.println("Hello from GFG class!");
+    }
+}
+class Main{   
+    public static void main(String[] args){
+        // GFG u = new GFG(); // Error: constructor is
+        // private
+       GFG.displayMessage();
+    }
+}
 
 
 //constructor overloading example
