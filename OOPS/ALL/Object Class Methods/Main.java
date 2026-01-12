4486aefc99635338c624e1d16400dfce444868e1
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-12 13:25:20 +0530

    java tutorial

--- OOPS/ALL/Object Class Methods/Main.java
+++ OOPS/ALL/Object Class Methods/Main.java
@@ -20,19 +20,63 @@
 // }
 
 
-class Book{
-    String title;
-    Book(String title) {
-        this.title = title;
-    }
+// class Book{
+//     String title;
+//     Book(String title) {
+//         this.title = title;
+//     }
+//     @Override
+//     public boolean equals(Object obj){   
+//         Book b = (Book) obj;
+//         return this.title.equals(b.title);
+//     }
+//     public static void main(String[] args) {
+//         Book b1 = new Book("Java");
+//         Book b2 = new Book("Java");
+//         System.out.println(b1.equals(b2)); // true
+//     }
+// }
+
+
+// class Geeks{
+//     public static void main(String[] args)
+//     {
+//         Object o = new String("GeeksForGeeks");
+//         Class c = o.getClass();
+//         System.out.println("Class of Object o is: "
+//                            + c.getName());
+//     }
+// }
+
+
+// class Geeks {
+//     public static void main(String[] args) {
+//         Geeks t = new Geeks();
+//         System.out.println(t.hashCode());
+//         t = null;
+//         // calling garbage collector
+//         System.gc();
+
+//         System.out.println("end");
+//     }
+//     @Override protected void finalize()
+//     {
+//         System.out.println("finalize method called");
+//     }
+// }
+
+
+class Student implements Cloneable{ 
+    int id = 1;
+    String name = "Vishnu";
     @Override
-    public boolean equals(Object obj){   
-        Book b = (Book) obj;
-        return this.title.equals(b.title);
+    public Object clone() throws CloneNotSupportedException{
+        return super.clone();  // shallow copy
     }
-    public static void main(String[] args) {
-        Book b1 = new Book("Java");
-        Book b2 = new Book("Java");
-        System.out.println(b1.equals(b2)); // true
+    public static void main(String[] args) throws Exception{   
+        Student s1 = new Student();
+        Student s2 = (Student) s1.clone();
+        System.out.println(s1.name); // Vishnu
+        System.out.println(s2.name); // Vishnu
     }
 }
