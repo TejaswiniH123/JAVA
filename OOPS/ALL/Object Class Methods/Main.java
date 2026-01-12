1c00308111dbc47ba94babc5d9046120a236dae6
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-12 13:06:59 +0530

    java tutorial

--- OOPS/ALL/Object Class Methods/Main.java
+++ OOPS/ALL/Object Class Methods/Main.java
@@ -1,20 +1,38 @@
-class Person{  
-    String n;  
-    // Constructor
-    public Person(String n) {
-        this.n = n;
+// class Person{  
+//     String n;  
+//     // Constructor
+//     public Person(String n) {
+//         this.n = n;
+//     }
+//     // Override toString() for a 
+//     // custom string representation
+//     @Override
+//     public String toString() {
+//         return "Person{name:'" + n + "'}";
+//     }
+//     public static void main(String[] args) { 
+//         Person p = new Person("Geek");
+//         // Custom string representation
+//         System.out.println(p.toString());
+//         // Default hash code value
+//         System.out.println(p.hashCode()); 
+//     }
+// }
+
+
+class Book{
+    String title;
+    Book(String title) {
+        this.title = title;
     }
-    // Override toString() for a 
-    // custom string representation
     @Override
-    public String toString() {
-        return "Person{name:'" + n + "'}";
+    public boolean equals(Object obj){   
+        Book b = (Book) obj;
+        return this.title.equals(b.title);
     }
-    public static void main(String[] args) { 
-        Person p = new Person("Geek");
-        // Custom string representation
-        System.out.println(p.toString());
-        // Default hash code value
-        System.out.println(p.hashCode()); 
+    public static void main(String[] args) {
+        Book b1 = new Book("Java");
+        Book b2 = new Book("Java");
+        System.out.println(b1.equals(b2)); // true
     }
 }
