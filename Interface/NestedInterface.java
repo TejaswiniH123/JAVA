de9f24e726eee546389f07423f624db224dc29ee
TejaswiniH123 <tejaswinih9@gmail.com>
2026-01-20 17:10:33 +0530

    java tutorial

--- Interface/NestedInterface.java
+++ Interface/NestedInterface.java
@@ -1,19 +1,19 @@
-//Nested Interface Inside a Class
-class Outer {
-    interface Inner {
-        void display();
-    }
-}
-class GFG implements Outer.Inner{   
-    public void display(){
-        System.out.println(
-            "Hello from Nested Interface Inside a Class!");
-    }
-    public static void main(String[] args){   
-        Outer.Inner obj = new GFG();
-        obj.display();
-    }
-}
+// //Nested Interface Inside a Class
+// class Outer {
+//     interface Inner {
+//         void display();
+//     }
+// }
+// class GFG implements Outer.Inner{   
+//     public void display(){
+//         System.out.println(
+//             "Hello from Nested Interface Inside a Class!");
+//     }
+//     public static void main(String[] args){   
+//         Outer.Inner obj = new GFG();
+//         obj.display();
+//     }
+// }
 
 
 //Nested Interface Inside an Interface
